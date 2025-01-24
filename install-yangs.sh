d1b2ade09171b2522b0c0057c6cf28084b39fbe1
Yunus Emre Ayhan <ayhanyunusemre@gmail.com>
2025-01-24 11:09:12 +0100

    devcontainer

---
+++ install-yangs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sysrepoctl --install /root/sysrepo-pg/yang/standard/ieee/published/802.1/ieee802-dot1q-bridge.yang \
+           --install /root/sysrepo-pg/yang/standard/ieee/published/802.1/ieee802-dot1q-vlan-bridge.yang \
+           --search-dirs /root/sysrepo-pg/yang/standard/ietf/RFC/:/root/sysrepo-pg/yang/standard/ieee/published/802/:/root/sysrepo-pg/yang/standard/ieee/published/802.1/:/root/sysrepo-pg/yang/standard/ieee/published/802.3/:/root/sysrepo-pg/yang/standard/ieee/published/802.11/:/root/sysrepo-pg/yang/standard/ieee/published/1588/:/root/sysrepo-pg/yang/standard/ieee/published/1906.1/ \
+            -v3
